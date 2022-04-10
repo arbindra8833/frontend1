91ca49a19b7fbb60d9a6437ab5c550aaae08307f
arbindra8833 <arbindra1sah@gmail.com>
2022-04-10 23:17:31 +0545

    frontend

---
+++ form.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+
+<body>
+    <form>
+        <div>
+            Name: <input type="text" name="myname">
+        </div>
+        <br>
+        <div>
+            Email: <input type="text" name="email">
+        </div>
+        <br>
+        <div>
+            Address: <input type="text" name="address">
+        </div>
+        <br>
+        <div>
+            phone number: <input type="text" name="number">
+        </div>
+        <br>
+        <div>
+            Bonus: <input type="number" name="my bonus">
+        </div>
+        <br>
+        <div>
+            Date: <input type="date" name="mydate">
+        </div>
+        <br>
+        <div>
+            Are you eligible: <input type="checkbox" name="my eligibility">
+        </div>
+        <br>
+        <div>
+            Gender: Male <input type="radio" name="mygender"> FeMale <input type="radio" name="mygender">
+        </div>
+        <div>
+            <input type="submit" value="submit now">
+        </div>
+        <div>
+            <input type="reset" value="reset now">
+        </div>
+    </form>
+</body>
+
+</html>
